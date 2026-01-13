12893ca3a2a64b372212fac74823e3b1f8ad86de
Shani Patil <shanipatil45@gmail.com>
2026-01-13 14:18:06 +0530

    Create SIP.java

---
+++ 35 | Search Insert Position /SIP.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        
+        if(target<=nums[0])
+            return 0;
+        else if(target>nums[nums.length-1])
+            return nums.length;
+        else if(target==nums[nums.length-1])
+            return nums.length-1;
+      
+        int low=0;
+        int high=nums.length-1;
+        int mid;
+        
+        while(low<=high){
+            mid=(low+high)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(target<nums[mid]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+    }
+     return low;
+}}
